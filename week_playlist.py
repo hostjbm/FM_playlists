b75e4e459e34477f6add1d19c90c396d5e1e7422
butko <test@test.com.ua>
2016-11-09 15:10:10 +0200

    Reduce code 2

--- week_playlist.py
+++ week_playlist.py
@@ -162,11 +162,11 @@ def save_playlist(st):
 
 if __name__ == "__main__":
 
-    # save_playlist('rus_radio')
-    # save_playlist('hit_fm')
-    # save_playlist('kiss_fm')
-    # save_playlist('lux_fm')
-    # save_playlist('nrj_fm')
+    save_playlist('rus_radio')
+    save_playlist('hit_fm')
+    save_playlist('kiss_fm')
+    save_playlist('lux_fm')
+    save_playlist('nrj_fm')
 
-    print(get_url('nrj_fm'))
+    # print(get_url('nrj_fm'))
     # get_playlist('http://lux.fm/player/airArchive.do?filter=2016090700', 'TEST', 'test.csv', "lux_fm")
