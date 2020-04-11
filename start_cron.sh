18ab0e3ad3fafdfd63aa5dec9cd35ecb78d97010
hostjbm <host_jbm@ukr.net>
2020-04-11 16:16:19 +0300

    Update start_cron.sh

--- start_cron.sh
+++ start_cron.sh
@@ -10,4 +10,3 @@ echo "##########################"
 echo "########## tar ###########"
 echo "##########################"
 find . -type f -name 'Week_*' -mtime 0 -exec tar -vtf {} \; | awk '{print $3,$5,$6}' | sort -n | grep -v '\/$'
-
