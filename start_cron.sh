c3e6ffe763271f36654cbfd78849c53f177371b0
butko <host_jbm@ukr.net>
2020-04-20 13:29:39 +0300

    Edit cron script

--- start_cron.sh
+++ start_cron.sh
@@ -4,9 +4,9 @@
 cd $(dirname $0)
 /usr/bin/python3 week_playlist.py
 
-find . -type d -name 'Week*' -exec tar -cvf {}.tar {} \;
+find . -type d -name 'Week*' -exec tar -cvfz {}.tar.gz {} \;
 find . -type d -name 'Week*' -exec rm -rf {} \;
-echo "##########################"
-echo "########## tar ###########"
-echo "##########################"
-find . -type f -name 'Week_*' -mtime 0 -exec tar -vtf {} \; | awk '{print $3,$5,$6}' | sort -n | grep -v '\/$'
+echo "#########################"
+echo "### Empty file in tar ###"
+echo "#########################"
+find . -type f -name 'Week_*' -mtime 0 -exec tar -vtf {} \; | awk '{if ($3 =="0") print $3,$5,$6}' | grep -v '\/$
