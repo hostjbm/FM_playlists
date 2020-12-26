7fa553acd51c16edee484c62872b7c85aedb41eb
Yaroslav <Yaroslav@my.com>
2020-12-26 20:09:12 +0200

    Adde thread into logging

--- week_playlist.py
+++ week_playlist.py
@@ -19,7 +19,7 @@ If need add new station firs off all add it to get url and after that to  get_pl
 '''
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s: [%(levelname)s] - %(funcName)s(%(lineno)d) - %(message)s',
+                    format='%(asctime)s: [%(levelname)s] - [%(threadName)s] %(funcName)s(%(lineno)d) - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S',
                     )
 def get_week():
