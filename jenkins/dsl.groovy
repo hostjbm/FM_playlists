55f7eb2d56abb6747e51496bc13fe7c5d7874849
hostjbm <host_jbm@ukr.net>
2021-02-23 19:35:35 +0200

    Change DSL

--- jenkins/dsl.groovy
+++ jenkins/dsl.groovy
@@ -13,7 +13,7 @@ job("FM_playlists") {
 	concurrentBuild(false)
 	steps {
 		shell("/usr/bin/python3 week_playlist.py")
-		shell("ls -la")
+		shell("find . -type d -name 'Week*' -exec tar -zcvf {}.tar.gz {} \;")
 	}
 	publishers {
 		archiveArtifacts {
